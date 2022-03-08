06dd686a62aeeffb690f953a624a9ccaaa524bda
lucianohuenuvil <lucianobvx@gmail.com>
2022-03-08 20:48:53 -0300

    Se añade funcionalidad de scroll
    Se elimina pagina de contacto y se añade al index

--- static/main.js
+++ static/main.js
@@ -56,3 +56,25 @@ function navresponsive(){
     }
     
 }
+
+
+//Slide scroll page
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+
+
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
